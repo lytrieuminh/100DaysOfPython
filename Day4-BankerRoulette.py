d402785e0271441120a8059b255c40926ad06253
lytrieuminh <lytrieuminh@gmail.com>
2023-01-15 23:34:23 +0200

    Day4

---
+++ Day4-BankerRoulette.py
@@ -0,0 +1,18 @@
+#Day 4: Banker Roulette
+
+import random
+
+names_string = input("Give me everybody's names, separated by a comma. \n")
+names = names_string.split(", ")
+
+how_many_names = len(names)
+random_name = random.randint(0,how_many_names - 1)
+
+#Solution 1:
+person_will_pay = names[random_name]
+print(person_will_pay + " is going to buy the meal today! Thank you!")
+
+#Another solution:
+#person_will_pay = random.choice(names)
+#print(person_will_pay + " is going to buy the meal today! Thank you!")
+
---
+++ Day4-HeadsOrTails.py
@@ -0,0 +1,11 @@
+#Day 4: Heads Or Tails
+
+import random
+
+a = random.randint(0,1)
+if a == 1:
+    print("Heads")
+else:
+    print("Tails")
+
+    
