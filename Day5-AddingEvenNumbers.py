8b4b8279dab91cc48128af0de6bba75bf20490d9
lytrieuminh <lytrieuminh@gmail.com>
2023-01-17 23:09:55 +0200

    Day5

---
+++ Day5-AddingEvenNumbers.py
@@ -0,0 +1,15 @@
+#Day 5: Adding Even Numbers:
+
+#Option 1:
+total1 = 0
+for number1 in range(2,101,2):
+  total1 += number1
+print(total1)
+
+#Option 2:
+total2 = 0
+for number2 in range(1,101):
+    if number2 %2 == 0:
+      total2 += number2
+print(total2)
+
---
+++ Day5-FizzBuzz.py
@@ -0,0 +1,12 @@
+#Day 5: FizzBuzz:
+
+for number in range(1,101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
+
